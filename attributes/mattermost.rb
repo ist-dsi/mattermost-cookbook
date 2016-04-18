7820b932d1f5077af7c7b619fbd201662ea1b312
Kyle Corupe <kyle.corupe@verifi.com>
2016-04-18 14:26:38 -0700

    updating mattermost version to 2.2.0

--- attributes/mattermost.rb
+++ attributes/mattermost.rb
@@ -1,5 +1,5 @@
-default['mattermost']['package']['url'] = "https://github.com/mattermost/platform/releases/download/v2.0.0/mattermost.tar.gz"
-default['mattermost']['package']['checksum'] = "005687c6a8128e1e40d01933f09d7da1a1b70b149a6bef96d923166bc1e7ce8f"
+default['mattermost']['package']['url'] = "https://releases.mattermost.com/2.2.0/mattermost-team-2.2.0-linux-amd64.tar.gz"
+default['mattermost']['package']['checksum'] = "d723fe9bf18d2d2a419a8d2aa6ad94fc99f251f8382c4342f08a48813501ca06"
 default['mattermost']['config']['install_path'] = "/opt" # installs to /opt/mattermost
 default['mattermost']['config']['user'] = "mattermost"
 default['mattermost']['config']['group'] = "mattermost"
