ea64f0d85e2082309143118a2a62cfcdd56d59f2
Grant Limberg <grant.limberg@zerotier.com>
2017-06-22 15:29:52 -0700

    fix json

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kyle.corupe@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures mattermost http://mattermost.com'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 %w(redhat centos ubuntu).each do |os|
     supports os
