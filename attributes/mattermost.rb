b0136561aebdf590670c6ed3ea4520657aff18cd
Simão Silva <simao.silva@tecnico.ulisboa.pt>
2019-06-24 22:47:48 +0100

    Upgrade to mattermost 5.12.0

--- attributes/mattermost.rb
+++ attributes/mattermost.rb
@@ -1,6 +1,6 @@
 default['mattermost']['package'] = {
-  'url' => 'https://releases.mattermost.com/5.11.1/mattermost-5.11.1-linux-amd64.tar.gz',
-  'checksum' => 'df1615d9c3bfe25c8d9dbad201e5f938bd40daaf93701d3954638212fb54425d',
+  'url' => 'https://releases.mattermost.com/5.12.0/mattermost-5.12.0-linux-amd64.tar.gz',
+  'checksum' => 'ce4b1b182e12ca499ba3e1a115f7635e0747430b741b3e10b4de54b8bcbfdfd7',
 }
 
 default['mattermost']['config'] = {
