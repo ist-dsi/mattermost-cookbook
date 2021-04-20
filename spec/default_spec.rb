81f039d6026a3bb8f2206870190e94e7df9ce13b
Simão Silva <simao.silva@tecnico.ulisboa.pt>
2021-04-20 13:08:38 +0100

    Fix typo

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,7 +8,7 @@ describe 'mattermost-cookbook::default' do
   end
 
   it 'downloads and extracts' do
-    expect(chef_run).to extract_tar_extract('https://releases.mattermost.com/5.18.2/mattermost-5.18.3-linux-amd64.tar.gz').with(
+    expect(chef_run).to extract_tar_extract('https://releases.mattermost.com/5.18.2/mattermost-5.18.2-linux-amd64.tar.gz').with(
       checksum: 'e61d6affca5bcf0e85b9152ff280b11135861f1b7b76dd30ad3ca96913c9f7a6',
       target_dir: '/opt',
       user: 'mattermost',
