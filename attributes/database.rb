e365b1d6e824d439a25528061307c24661ca6a26
Simão Silva <simao.silva@tecnico.ulisboa.pt>
2021-04-20 11:43:38 +0100

    Closes #63, closes #64

--- attributes/database.rb
+++
@@ -1,7 +0,0 @@
-default['mattermost']['database'] = {
-  'address' => 'dockerhost',
-  'port' => 3306,
-  'name' => 'mattermost_test',
-  'username' => 'mmuser',
-  'password' => 'mostest',
-}
--- test/cookbooks/mysql_test/recipes/default.rb
+++ test/cookbooks/mysql_test/recipes/default.rb
@@ -1,11 +1,20 @@
 mariadb_repository 'install'
 
 mariadb_server_install 'mariadb server' do
-  action [:install, :create]
-  password ''
+  action :install
 end
+
 mariadb_client_install 'mariadb client' do
   version '10.3'
+end
+
+service 'mysql' do
+  action :start
+end
+
+mariadb_server_install 'mariadb server' do
+  action :create
+  password ''
 end
 
 mariadb_database 'mattermost_test' do
